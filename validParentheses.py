75972aea782ed1c14814be415aeddabab9b52c46
basonchuang <giveup20000813@gmail.com>
2022-11-22 11:33:21 +0800

    Copy one of the solution from leetcode, find out that I'm not familiar with those skills to dealing with those problem...

--- validParentheses.py
+++ validParentheses.py
@@ -1,57 +1,14 @@
 class Solution(object):
-    def f(x):
-        return {
-            'a': 1,
-            'b': 2,
-        }[x]
     def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
-        n_of_condition = []
-        if "(" in s:
-            left = s.index("(")
-            right = s.find(")")
-            if right == -1:
-                n_of_condition.append(False)
-            elif right == left + 1:
-                n_of_condition.append(True)
-            elif (right - left) % 2 == 1:
-                n_of_condition.append(True)
-            else:
-                n_of_condition.append(False)
-        if "[" in s:
-            left = s.index("[")
-            right = s.find("]")
-            if right == -1:
-                n_of_condition.append(False)
-            elif right == left + 1:
-                n_of_condition.append(True)
-            elif (right - left) % 2 == 1:
-                n_of_condition.append(True)
-            else:
-                n_of_condition.append(False)
-        if "{" in s:
-            left = s.index("{")
-            right = s.find("}")
-            if right == -1:
-                n_of_condition.append(False)
-            elif right == left + 1:
-                n_of_condition.append(True)
-            elif (right - left) % 2 == 1:
-                n_of_condition.append(True)
-            else:
-                n_of_condition.append(False)
-
-        count = 0
-        for i in n_of_condition:
-            if i:
-                count+=1
-        if count != 0 and count == len(n_of_condition):
-            return True
-        else:
-            return False
-sol = Solution()
-ans = sol.isValid("(){}}{")
-print(ans)
+        stack, match = [], {"]": "[", "}": "{", ")": "("}
+        for char in s:
+            if char in match:
+                if not (stack and stack.pop() == match[char]):
+                    return False
+                else:
+                    stack.append(char)
+        return not stack
